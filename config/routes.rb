dc191e90418f75a3078b896c6843ee052ad08192
cmcorrales <cmcorrales3@gmail.com>
2018-08-22 07:01:12 -0400

    reset schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,4 @@
 Rails.application.routes.draw do
-  get 'comments/new'
-  get 'comments/create'
-  get 'comments/edit'
-  get 'comments/destroy'
-  get 'questions/index'
-  get 'questions/show'
-  get 'questions/new'
-  get 'questions/edit'
-  get 'questions/update'
-  get 'questions/destroy'
-  get '/static_pages/home'
 
-  get    '/signup',  to: 'users#new'
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#login'
-  get '/logout',  to: 'sessions#logout'
-  resources :users
-  resources :questions do
-
-  end
-  get "/questions/:course_id", "questions#index", as:"questions"
-  get "/questions/:course_id/:question_id", "questions#show", as:"question"
-  resources :comments, only: [:new, :create, :delete]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,32 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_21_200855) do
-
-  create_table "comments", force: :cascade do |t|
-    t.integer "question_id"
-    t.text "content"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "questions", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.integer "course_id"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "type"
-    t.string "user_name"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
